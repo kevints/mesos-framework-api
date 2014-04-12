2902debf282be790a1f63848f9be8cd73f33001d
Kevin Sweeney <kevin.t.sweeney@gmail.com>
2014-04-12 14:56:16 -0700

    Initial commit.

---
+++ src/test/java/com/github/kevints/mesos/MessagesTest.java
@@ -0,0 +1,13 @@
+package com.github.kevints.mesos;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class MessagesTest {
+  @Test
+  public void testCompile() {
+    // No-op to verify that we did compile.
+    assertNotNull(Messages.class);
+  }
+}
